0ed373b6397924c56be96be4576787ce28e39db5
kariya <seiriseitonn47@gmail.com>
2023-12-23 18:38:45 +0900

    add swerve_drive_control and drobo_interfaces for submodule

--- install/swerve_drive_control/share/swerve_drive_control/rust/src/main.rs
+++ install/swerve_drive_control/share/swerve_drive_control/rust/src/main.rs
@@ -2,7 +2,6 @@ use safe_drive::{
     context::Context, error::DynError, logger::Logger, pr_info
 };
 
-
 use num_traits::pow::Pow;
 
 use core::f64::consts::PI;
@@ -42,7 +41,7 @@ fn move_chassis(_xrpm: f64, _yrpm: f64, _yaw: f64) {
     let rad: f64 = _yrpm.atan2(_xrpm);
     
     //回転成分　θ・R _yawの０基準を前方にしてロボットからの距離をかける
-    let rotation_component: f64 = _yaw * ROBOT_CENTER_TO_WHEEL_DISTANCE;
+    let rotation_component: f64 = (_yaw - PI/2.0) * ROBOT_CENTER_TO_WHEEL_DISTANCE;
 
     //各ホイールのx,y成分を出す。並進成分と回転成分を合成
     let front_left_component = Component{
@@ -63,10 +62,10 @@ fn move_chassis(_xrpm: f64, _yrpm: f64, _yaw: f64) {
     };
 
     //各ホイールの方向を出す
-    let front_left_direction: f64 = front_left_component.y.atan2(front_left_component.x);
-    let front_right_direction: f64 = front_right_component.y.atan2(front_right_component.x);
-    let rear_left_direction: f64 = rear_left_component.y.atan2(rear_left_component.x);
-    let rear_right_direction: f64 =rear_right_component.y.atan2(rear_right_component.x);
+    let front_left_yaw: f64 = front_left_component.y.atan2(front_left_component.x);
+    let front_right_yaw: f64 = front_right_component.y.atan2(front_right_component.x);
+    let rear_left_yaw: f64 = rear_left_component.y.atan2(rear_left_component.x);
+    let rear_right_yaw: f64 =rear_right_component.y.atan2(rear_right_component.x);
 
 
     let front_left_speed: i64 = ((front_left_component.x.powf(2.0) + front_left_component.y.powf(2.0)).sqrt() / WHEEL_DIA) as i64;
@@ -74,13 +73,13 @@ fn move_chassis(_xrpm: f64, _yrpm: f64, _yaw: f64) {
     let rear_left_speed: i64 = ((rear_left_component.x.powf(2.0) + rear_left_component.y.powf(2.0)).sqrt() / WHEEL_DIA) as i64;
     let rear_right_speed: i64 = ((rear_right_component.x.powf(2.0) + rear_right_component.y.powf(2.0)).sqrt() / WHEEL_DIA) as i64;
 
-    move_wheel(front_left_direction, front_left_speed);
-    move_wheel(front_right_direction, front_right_speed);
-    move_wheel(rear_left_direction, rear_left_speed);
-    move_wheel(rear_right_direction, rear_right_speed);
+    move_wheel(front_left_yaw, front_left_speed);
+    move_wheel(front_right_yaw, front_right_speed);
+    move_wheel(rear_left_yaw, rear_left_speed);
+    move_wheel(rear_right_yaw, rear_right_speed);
 }
 
-fn move_wheel(_wheel_direction: f64, _power: i64){
+fn move_wheel(_wheel_yaw: f64, _power: i64){
     //いい感じに制御しよう
 
 }
